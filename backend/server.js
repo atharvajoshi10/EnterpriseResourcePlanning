1210631d9f7a9f0e53947deccbe5075faf27e0e6
Atharva Joshi <athu200299@gmail.com>
2020-04-25 17:13:46 +0530

    Added Coments from better explaination

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
- 
+/*
+*Server script for Nodejs. Use nodemon server to start it.
+*/
+
+//Dependencies
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+//Importing Enviornment File
 require('dotenv').config();
 
+//Starting Express server on port 5000
 const app = express()
 const port = process.env.PORT || 5000;
 
+//Ensuring Cors is used and server parses json files
 app.use(cors())
 app.use(express.json())
 
+
+//Establishing connection with the mongodb cluster
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { 
     useNewUrlParser: true,
@@ -22,6 +31,7 @@ connection.once('open', () => {
     console.log("MongoBD connection established");
 })
 
+//Bind the server to listen on port.
 app.listen(port, () =>{
     console.log(`Server is running on port: ${port}`);
 })
