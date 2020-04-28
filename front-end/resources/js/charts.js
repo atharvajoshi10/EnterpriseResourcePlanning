37483eafc62fc1e3f8248ba11d558fb9c45a7f96
Megh <meghkhaire@gmail.com>
2020-04-28 19:14:03 +0530

    front-end

---
+++ front-end/resources/js/charts.js
@@ -0,0 +1,25 @@
+
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses', 'Profit'],
+    ['2014', 1000, 400, 200],
+    ['2015', 1170, 460, 250],
+    ['2016', 660, 1120, 300],
+    ['2017', 1030, 540, 350]
+  ]);
+
+  var options = {
+    chart: {
+      title: 'Company Performance',
+      subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+    }
+  };
+
+  var chart = new google.charts.Bar(document.getElementById('columnchart_material1'));
+  var chart2 = new google.charts.Bar(document.getElementById('columnchart_material2'));
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+  chart2.draw(data, google.charts.Bar.convertOptions(options));
+}
---
+++ front-end/resources/js/dashboard.js
@@ -0,0 +1,8 @@
+/* NAVIGATION */
+$(".nav-btn").on('click',function(){
+   $(".sidebar").toggleClass("is-open");
+});
+
+$(".collapse-btn").on('click',function(){
+   $(".sidebar").toggleClass("is-open");
+})
