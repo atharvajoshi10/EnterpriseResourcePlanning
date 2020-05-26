1f6ecbddfa2b5ed70a5c9cc25c14c9ca8ccbad32
Megh <meghkhaire@gmail.com>
2020-05-26 19:37:21 +0530

    Process Controller

--- erp/controllers/process.controller.js
+++ erp/controllers/process.controller.js
@@ -4,6 +4,9 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const multer = require('multer');
 
+//########## VIEWS CONTROLLERS ##########
+
+//Render Process List View 
 exports.getProcesses = catchAsync(async (req,res,next) => {
     const processes = await Process.find().sort('-createdAt');
     res.render('processList',{
@@ -12,6 +15,7 @@ exports.getProcesses = catchAsync(async (req,res,next) => {
     });
 });
 
+//Render Selected Process
 exports.getProcessById = catchAsync(async (req,res,next) =>{
     const process = await Process.findById(req.params.id)
     if(!process){
@@ -23,49 +27,35 @@ exports.getProcessById = catchAsync(async (req,res,next) =>{
     });
 });
 
-// exports.updateProcess = catchAsync(async (req,res,next) =>{
-//     const process = await Process.findById(req.params.id)
-//     if(!process){
-//         return next(new AppError('No Process found with that ID',404));
-//     }
-//     res.render('editprocess',{
-//         title: 'Edit Process | ' + process.process_name,
-//         process
-//     });
-// })
+//########## API CONTROLLERS ##########
 
-// exports.addProcess = catchAsync(async (req, res, next) => {
-//     res.render('createProcess',{
-//         title: 'Create new Process'
-//     });
-// });
-
-//##########API#########
+//Api to update selected process
 exports.updateProcessApi = catchAsync(async (req,res,next) =>{
     const process = await Process.findById(req.params.id);
     if(!process){
         return next(new AppError('No Process found with that ID',404));
     }
-    process.process_name = req.body.process_name;
-    process.process_id = req.body.process_id;
-    process.description = req.body.description;
-    process.worker_id = req.body.worker_id;
-    process.machine_id = req.body.machine_id;
-    process.process_thumbnail_location = req.body.process_thumbnail_location;
+    const updates = Object.keys(req.body)
+    updates.forEach((update) => process[update] = req.body[update])
     process.username_updated=req.employee.e_username;
     await process.save()
     res.json({status:'success'});
 });
 
+//Api to save new process
 exports.saveNewProcessApi = catchAsync(async (req,res,next) =>{
     const newProcess = new Process({
         ...req.body,
         username_created: req.employee.e_username
     });
     const process = await newProcess.save()
-    res.json({status:'success',id: process._id});
+    res.json({
+        status:'success',
+        id: process._id
+    });
 });
 
+//Api to delete selected process
 exports.deleteProcessApi = catchAsync(async(req,res,next) => {
     const process = await Process.findByIdAndDelete(req.params.id)
     if(!process){
@@ -75,16 +65,19 @@ exports.deleteProcessApi = catchAsync(async(req,res,next) => {
 });
 
 /*##########Image Upload Controllers###########*/
-
+//Function to set destination an filename of thumbnail image
 const multerImgStorage = multer.diskStorage({
     destination: (req,file,cb) =>{
         cb(null,'public/img/process');
     },
     filename: (req,file,cb) => {
-        cb(null,file.originalname);
+        const ext = file.mimetype.split('/')[1];
+        const filename=file.originalname.split('.')[0];
+        cb(null,`thumbnail-${filename}-${Date.now()}.${ext}`);
     }
 });
 
+//Function to filter uploaded file (Validation Check)
 const multerImgFilter = (req,file,cb) => {
     //Image FILES FILTERING
     if(!file.mimetype.startsWith('image')){
@@ -93,6 +86,7 @@ const multerImgFilter = (req,file,cb) => {
     cb(null, true)
 }
 
+//Function to upload Validated File
 uploadImg = multer({
     storage: multerImgStorage,
     limits: {
@@ -101,18 +95,14 @@ uploadImg = multer({
     fileFilter: multerImgFilter
 });
 
+//Api to upload image on local storage
 exports.uploadImage = uploadImg.single('thumbnail');
+
+//Api to get Location of uploaded image
 exports.getImageLocation = catchAsync(async (req,res,next) =>{
-    const location = '/img/process/'+req.file.originalname;
+    const location = `/img/process/${req.file.filename}`;
     res.json({
         status:'success',
         location: location
     });
 });
-
-// exports.saveImageLocation = catchAsync(async (req,res,next) =>{
-//     const location = '/img/items/'+req.file.originalname;
-//     const item = await Item.findById(req.params.id)
-//     item.item_thumbnail_location = location;
-//     await item.save()
-// });
