84e9e6ba8f13d9bc8d1ec63acb654b8914acbc3d
Megh <meghkhaire@gmail.com>
2020-05-28 00:58:48 +0530

    Process Routes

--- erp/routes/process.routes.js
+++ erp/routes/process.routes.js
@@ -9,14 +9,12 @@ const auth = require('../middleware/auth')
 
 //Process Api Routes
 router.patch('/api/update/:id',auth, processController.updateProcessApi);
-router.post('/api/add',auth, processController.saveNewProcessApi);
+router.post('/api/create',auth, processController.saveNewProcessApi);
 router.delete('/api/delete/:id',auth, processController.deleteProcessApi);
 router.post('/api/uploadImage', auth, processController.uploadImage, processController.getImageLocation);
 
 //Process View Routes
 router.get('/', auth, processController.getProcesses);
-// router.get('/add',auth, processController.addProcess);
 router.get('/:id',auth, processController.getProcessById);
-// router.get('/update/:id',auth, processController.updateProcess);
 
 module.exports = router;
